9563cc8446cff9ec64dfc31b7a002a31c936d910
kevinmulier <kevin.mulier@outlook.fr>
2023-03-16 00:02:22 +0100

    Added colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      fublu: {
+        50: "#eff1fe",
+        100: "#e2e4fd",
+        200: "#cacdfb",
+        300: "#aaadf7",
+        400: "#8d88f1",
+        500: "#796be9",
+        600: "#6a4fdc",
+        700: "#644ac5",
+        800: "#4b369d",
+        900: "#3f337c",
+      },
+    },
     extend: {},
   },
   plugins: [],
