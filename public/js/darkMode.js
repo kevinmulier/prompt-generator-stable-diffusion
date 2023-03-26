b912984615d907e220825793552e7b3cc19622b9
Siddhanta Paul <siddhantapaul26@gmail.com>
2023-03-26 08:22:31 +0530

    feat: improved the copy button color, included darkmode.js as a module into main.js

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,15 +1,13 @@
-const toggleThemeBtn = document.querySelectorAll('.toggleThemeBtn');
-
 function toggleTheme() {
-  const html = document.documentElement;
-  const currentTheme = localStorage.getItem('theme');
-  if (currentTheme === 'dark') {
-    localStorage.setItem('theme', 'light');
-    html.classList.remove('dark');
-  } else {
-    localStorage.setItem('theme', 'dark');
-    html.classList.add('dark');
-  }
+	const html = document.documentElement;
+	const currentTheme = localStorage.getItem("theme");
+	if (currentTheme === "dark") {
+		localStorage.setItem("theme", "light");
+		html.classList.remove("dark");
+	} else {
+		localStorage.setItem("theme", "dark");
+		html.classList.add("dark");
+	}
 }
 
-toggleThemeBtn.forEach((btn => btn.addEventListener('click', toggleTheme)));
+export default toggleTheme;
